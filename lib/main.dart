da24f16b19a08400d90be47a1645892120de5b3a
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-05-27 17:00:24 -0300

    Mais mudanças de tema

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Gerador de nome para Startup',
       theme: ThemeData(//configuração do tema
+        listTileTheme: const ListTileThemeData( //tema do listTile
+          tileColor: Colors.white10, //cor de cada tile
+          textColor: Colors.black, //cor da fonte dentro de cada tile
+        ),
+        dividerColor: Colors.amber, //cor do divisor
         appBarTheme: const AppBarTheme( //configuração do tema da barra
-          backgroundColor: Colors.white, //o fundo ta branco
-          foregroundColor: Colors.black,
+          backgroundColor: Colors.black, //o fundo ta branco
+          foregroundColor: Colors.white,
           shadowColor: Colors.purple, //a cor das sombras
           elevation: 3, //define o tamanho das sombras
         ),
